bbf0980d6fac10cb860136113a000763254c3575
Angelos Chalaris <chalarangelo@gmail.com>
2019-05-30 20:47:46 +0300

    Update templates

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 const PostCard = ({ post }) => {
   const url = `/${post.fields.slug}/`
