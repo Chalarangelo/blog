a766cd4b0818e7607e00041d8a02278420350e6a
Angelos Chalaris <chalarangelo@gmail.com>
2019-05-31 15:27:04 +0300

    Fix website

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,6 @@ module.exports = {
         purgeOnly: ['/all.scss'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    // 'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
