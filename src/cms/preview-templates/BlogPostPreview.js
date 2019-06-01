95bd49d475a46ac057b8ab3b0951f5cd55ba2ec1
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-01 11:59:43 +0300

    Update CMS

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,20 +1,20 @@
 import React from 'react'
+import '../../styles/all.scss'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  console.log(widgetFor('body'))
   return (
-  <div className="container" style={{ minHeight: '100vh' }}>
-    <article className="content">
-      <section className="post-full-content">
-        <h1 className="content-title">{entry.getIn(['data', 'title'])}</h1>
-        <p className="content-meta">{`${entry.getIn(['data', 'date'])}`}</p>
-        <section 
-          className="content-body load-external-scripts" >
-            {widgetFor('body')}
+    <div className="container" style={{ minHeight: '100vh' }}>
+      <article className="content">
+        <section className="post-full-content">
+          <h1 className="content-title">{entry.getIn(['data', 'title'])}</h1>
+          <p className="content-meta">{`${entry.getIn(['data', 'date'])}`}</p>
+          <section 
+            className="content-body load-external-scripts" >
+              {widgetFor('body')}
+          </section>
         </section>
-      </section>
-    </article>
-  </div>
+      </article>
+    </div>
 )}
 
 export default BlogPostPreview
